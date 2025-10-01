78410716af7fddbb90f5b72e9653741273556dd6
manaf <sgmalajl@liverpool.ac.uk>
2025-10-01 14:53:44 +0300

    Fix: remove ErrorBoundary import from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
-import ErrorBoundary from './components/ErrorBoundary'
 import './index.css'
 
 const queryClient = new QueryClient({
@@ -19,9 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <ErrorBoundary>
-          <App />
-        </ErrorBoundary>
+        <App />
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
